3271b3f6a3a15a42ed4fbfcef75ab8b11a6a4b09
Mohd Shadab <shadab_ibrahim@outlook.com>
2024-06-19 15:53:19 +0530

    submitting code-Wed Jun 19 2024 15:53:18 GMT+0530 (India Standard Time)

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -4,11 +4,12 @@ function WeatherDisplay({ temperature, condition }) {
     console.log(temperature)
     console.log(condition)
     temperature =25;
+    condition= "Sunny";
 
     return (
         <div>
            <p>Temperature: <span style={{ color: temperature > 20 ? 'red' : 'blue' }}>{temperature}</span></p>
-            <p>Condition: {condition}</p>
+            <p>Conditions: {condition}</p>
 
         </div>
     )
